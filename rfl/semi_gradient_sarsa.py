220a9011a04a52286aa01d1758b3458a6c665d2f
Theomat <theomatricon@gmail.com>
2020-11-10 13:54:02 +0100

    Fixed indexing issues

--- rfl/semi_gradient_sarsa.py
+++ rfl/semi_gradient_sarsa.py
@@ -14,18 +14,18 @@ class SemiGradientSARSALearner(AbstractModelLearner):
 
     def episode_to_dataset(self, episode: Episode) -> List[Tuple[np.ndarray, np.ndarray]]:
         dataset = []
-        T = len(episode) - 1
-        n = self.steps
-        tau = 0
-        t = 0
+        T: int = len(episode) - 1
+        n: int = self.steps
+        tau: int = 0
+        t: int = n - 1  # t in [n- 1; T - 2 + n]
         rewards = [r for (s, a, r) in episode]
         values = self.value_of_states([state for (state, a, r) in episode[n:]])
+        # len(values) = T + 1 - n
         while tau != T - 1:
-            tau = t - n + 1
-            if tau >= 0:
-                G = np.sum([self.gamma**(i-tau-1) * rewards[i] for i in range(tau + 1, min(tau + n, T) + 1)])
-                if tau + n < T:
-                    G += self.gamma**n * values[t]
-                dataset.append((episode[tau][0], G))
+            G = np.sum([self.gamma**(i-tau-1) * rewards[i] for i in range(tau + 1, min(tau + n, T) + 1)])
+            if tau + n < T:
+                G += self.gamma**n * values[t - n + 1]
+            dataset.append((episode[tau][0], G))
             t += 1
+            tau = t - n + 1
         return dataset
