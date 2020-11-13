Fixed off by one error

rfl/semi_gradient_sarsa.py
@@ -54,8 +54,8 @@ class SemiGradientSARSALearner(AbstractStateModelLearner):
     def _transition_to_dataset_(self, transition: SARSTuple) -> Tuple[np.ndarray, float, Optional[float]]:
         (state, action, reward, afterwards, w) = transition
         T: int = len(afterwards)
-        rewards = [r for (s, a, r) in afterwards]
-        G = np.sum([self.gamma**(i) * rewards[i] for i in range(min(T, self.steps - 1))])
+        G = reward + np.sum([self.gamma**(i + 1) * r for i, (s, a, r) in enumerate(afterwards[:-1])])
         if T == self.steps:
-            G += self.gamma**self.steps * self.value_of_state(afterwards[-1][0])
+            last_state = afterwards[-1][0].copy()
+            G += self.gamma**self.steps * self.value_of_state(last_state).detach().numpy()
         return state, G, w

rfl/uniform_replay_buffer.py
@@ -29,11 +29,11 @@ class UniformReplayBuffer(AbstractReplayBuffer):
         memories = self.generator.integers(0, len(self._memory), size, dtype=np.int)
         output = []
         for g_index in memories:
-            (episode_index, index, state, action, reward) = self._memory[g_index]
+            (episode_index, memory_index, state, action, reward) = self._memory[g_index]
             afterwards = []
             episode = self._episodes[episode_index]
-            i = index
-            for j in range(1, nsteps):
+            i = memory_index
+            for j in range(1, nsteps + 1):
                 if i + j < len(episode):
                     afterwards.append(episode[i + j])
             output.append((state, action, reward, afterwards, None))