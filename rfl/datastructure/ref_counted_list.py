746dffd86ae42abc066636abc95afeeb4023a415
Theomat <theomatricon@gmail.com>
2020-11-14 18:51:01 +0100

    Added RefCountedList to free unused episodes

---
+++ rfl/datastructure/ref_counted_list.py
@@ -0,0 +1,43 @@
+
+
+from typing import Dict, List, Any
+
+UID = int
+
+
+class RefCountedList():
+
+    def __init__(self):
+        self._dict: Dict[UID, Any] = {}
+        self._max_uid: UID = 0
+        self._free_uid: List[UID] = []
+
+    def __get_uid(self) -> UID:
+        if self._free_uid:
+            return self._free_uid.pop()
+        uid: UID = self._max_uid
+        self._max_uid += 1
+        return uid
+
+    def append(self, element: Any, refs: int = 1) -> UID:
+        uid: UID = self.__get_uid()
+        self._dict[uid] = [element, refs]
+        return uid
+
+    def increase_refs(self, uid: UID, new_refs: int = 1):
+        self._dict[uid][1] += new_refs
+
+    def decrease_refs(self, uid: UID, removed_refs: int = 1) -> bool:
+        tup = self._dict[uid]
+        tup[1] -= removed_refs
+        if tup[1] <= 0:
+            del self._dict[uid]
+            self._free_uid.append(uid)
+            return True
+        return False
+
+    def tolist(self) -> List[Any]:
+        return [el for (el, _) in self._dict.values()]
+
+    def __getitem__(self, key: UID):
+        return self._dict[key]
--- rfl/uniform_replay_buffer.py
+++ rfl/uniform_replay_buffer.py
@@ -1,5 +1,6 @@
 from rfl.env.abstract_environment import Episode
 from rfl.abstract_replay_buffer import AbstractReplayBuffer, SARSTuple
+from rfl.datastructure.ref_counted_list import RefCountedList
 
 from typing import List
 
@@ -12,26 +13,27 @@ class UniformReplayBuffer(AbstractReplayBuffer):
         self._size: int = size
         self._memory: List = []
         self.generator: np.random.Generator = np.random.default_rng(seed)
-        self._episodes: List[Episode] = []
+        self._episodes: RefCountedList = RefCountedList()
 
     def store(self, episodes: List[Episode]):
-        i = len(self._episodes)
         for episode in episodes:
+            uid = self._episodes.append(episode, len(episode))
             T = len(episode) - 1
             for j, (state, action, reward) in enumerate(reversed(episode)):
-                self._memory.append((i, T - j, state, action, reward))
+                self._memory.append((uid, T - j, state, action, reward))
             self._episodes.append(episode)
-            i += 1
         if len(self._memory) > self._size:
+            for (uid, _, _, _, _) in self._memory[:-self._size]:
+                self._episodes.decrease_refs(uid, 1)
             self._memory = self._memory[-self._size:]
 
     def sample(self, size: int, nsteps: int) -> List[SARSTuple]:
         memories = self.generator.integers(0, len(self._memory), size, dtype=np.int)
         output = []
         for g_index in memories:
-            (episode_index, memory_index, state, action, reward) = self._memory[g_index]
+            (episode_uid, memory_index, state, action, reward) = self._memory[g_index]
             afterwards = []
-            episode = self._episodes[episode_index]
+            episode = self._episodes[episode_uid]
             i = memory_index
             for j in range(1, nsteps + 1):
                 if i + j < len(episode):
